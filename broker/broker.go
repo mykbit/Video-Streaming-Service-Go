31095fe7957d5ca6bacba6ac54e8328645ea595d
mykbit <mbitiutskyy@gmail.com>
2023-10-13 02:28:44 +0100

    Add subscription system

--- broker/broker.go
+++ broker/broker.go
@@ -3,92 +3,127 @@ package main
 import (
 	"fmt"
 	"net"
+	_ "net/http/pprof"
 	"os"
-	"time"
+	"sync"
 )
 
 var println = fmt.Println
 var Sprintf = fmt.Sprintf
+var printf = fmt.Printf
+var wg sync.WaitGroup
+
+type SubscriberManager struct {
+	streams map[int32][]*net.UDPAddr
+	mu      sync.RWMutex
+}
+
+func NewSubscriberManager() *SubscriberManager {
+	return &SubscriberManager{
+		streams: make(map[int32][]*net.UDPAddr),
+	}
+}
+
+func (sm *SubscriberManager) AddSubscriber(producerID int32, sub *net.UDPAddr) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	sm.streams[producerID] = append(sm.streams[producerID], sub)
+	printf("Added subscriber to producer 0x%X\n", producerID)
+}
+
+func (sm *SubscriberManager) RemoveSubscriber(producerID int32, sub *net.UDPAddr) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	for i, addr := range sm.streams[producerID] {
+		if addr.IP.Equal(sub.IP) && addr.Port == sub.Port {
+			sm.streams[producerID] = append(sm.streams[producerID][:i], sm.streams[producerID][i+1:]...)
+		}
+	}
+}
+
+func (sm *SubscriberManager) GetSubscribers(producerID int32) []*net.UDPAddr {
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
+
+	return sm.streams[producerID]
+}
 
 func main() {
-	brokerAddressPub := os.Getenv("BROKER_PUB_ADDRESS")
-	brokerAddresPrivate := os.Getenv("BROKER_PRIVATE_ADDRESS")
 
-	udpBrokerAddrPub, err := net.ResolveUDPAddr("udp", brokerAddressPub)
+	brokerAddress := os.Getenv("BROKER_ADDRESS")
+
+	udpBrokerAddr, err := net.ResolveUDPAddr("udp", brokerAddress)
 	if err != nil {
 		println("Error resolving public broker address: ", err.Error())
 		os.Exit(0)
 	}
 
-	udpBrokerAddrPrivate, err := net.ResolveUDPAddr("udp", brokerAddresPrivate)
-	if err != nil {
-		println("Error resolving private broker address: ", err.Error())
-		os.Exit(0)
-	}
-
-	socketPublic, err := net.ListenUDP("udp", udpBrokerAddrPub)
+	socket, err := net.ListenUDP("udp", udpBrokerAddr)
 	if err != nil {
 		println("Error broker listening to public port: ", err.Error())
 		os.Exit(0)
 	}
-	println("Public socket established.")
-	defer socketPublic.Close()
+	println("Socket established.")
+	defer socket.Close()
 
-	socketPrivate, err := net.ListenUDP("udp", udpBrokerAddrPrivate)
-	if err != nil {
-		println("Error broker listening to private port: ", err.Error())
-		os.Exit(0)
-	}
-	println("Private socket established.")
-	defer socketPrivate.Close()
+	subscriberManager := NewSubscriberManager()
 
-	addrClient, err := connectToClient(socketPublic)
-	for err != nil {
-		println("Error connecting to client: ", err.Error())
-		addrClient, err = connectToClient(socketPublic)
-	}
+	wg.Add(1)
+	go acceptData(socket, subscriberManager)
+	wg.Wait()
+}
 
+func acceptData(socket *net.UDPConn, sm *SubscriberManager) {
+	defer wg.Done()
 	for {
-		acceptData(socketPrivate)
-		streamData(socketPublic, addrClient)
-		time.Sleep(6 * time.Second)
-	}
+		buffer := make([]byte, 65000)
 
+		n, addrClient, err := socket.ReadFromUDP(buffer)
+		if err != nil {
+			println("Error reading from client: ", err.Error())
+			return
+		}
+
+		dataBuffer := make([]byte, n)
+		copy(dataBuffer, buffer[:n])
+
+		event, fullProdID := decode(dataBuffer[:5])
+
+		switch event {
+		case 0:
+			printf("Producer 0x%X is streaming data.\n", fullProdID)
+			subscribers := sm.GetSubscribers(fullProdID)
+			go streamData(socket, subscribers, dataBuffer)
+		case 1:
+			printf("Client initiated subscription to the producer 0x%X\n", fullProdID)
+			sm.AddSubscriber(fullProdID, addrClient)
+		case 2:
+			printf("Client initiated unsubscription from the producer 0x%X\n", fullProdID)
+			sm.RemoveSubscriber(fullProdID, addrClient)
+		}
+	}
 }
 
-func connectToClient(socket *net.UDPConn) (*net.UDPAddr, error) {
-	buffer := make([]byte, 1024)
-
-	_, addrClient, err := socket.ReadFromUDP(buffer)
-	if err != nil {
-		println("Error reading from client: ", err.Error())
-		return nil, err
+func streamData(socket *net.UDPConn, subscribers []*net.UDPAddr, buffer []byte) {
+	for _, addr := range subscribers {
+		_, err := socket.WriteToUDP(buffer, addr)
+		if err != nil {
+			println("Error sending message to client: ", err.Error())
+		} else {
+			printf("Sending data to client %v\n", addr)
+		}
 	}
-	println("Client connected: ", addrClient.String())
-	return addrClient, nil
 }
 
-func streamData(socket *net.UDPConn, addrClient *net.UDPAddr) {
-	message := "Data from the server."
+func decode(buffer []byte) (int8, int32) {
+	event := int8(buffer[0])
 
-	data, err := socket.WriteToUDP([]byte(message), addrClient)
-	if err != nil {
-		println("Error sending message to client: ", err.Error())
-	}
-	time := time.Now().Format(time.ANSIC)
-	s := Sprintf("Sent from broker at %v: %v", time, data)
-	println(s)
-}
+	fullProdID := int32(buffer[1])<<24 |
+		int32(buffer[2])<<16 |
+		int32(buffer[3])<<8 |
+		int32(buffer[4])
 
-func acceptData(socket *net.UDPConn) {
-	buffer := make([]byte, 1024)
-
-	n, _, err := socket.ReadFromUDP(buffer)
-	if err != nil {
-		println("Error reading from client: ", err.Error())
-		return
-	}
-	time := time.Now().Format(time.ANSIC)
-	s := Sprintf("Received from producer at %v: bytes %v", time, n)
-	println(s)
+	return event, fullProdID
 }
